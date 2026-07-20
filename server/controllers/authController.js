0761885f124720af6b44836fe571e0ba7eb6ecea
Yogesh1212-eng <yogeshmaurya1205@gmail.com>
2026-07-21 00:01:25 +0530

    initial commit

---
+++ server/controllers/authController.js
@@ -0,0 +1,48 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+export const registerUser = async (req, res) => {
+    try {
+    const { name, email, password, role } = req.body;
+
+    // Check if all fields are provided
+    if (!name || !email || !password) {
+        return res.status(400).json({
+        success: false,
+        message: "Please fill all required fields",
+        });
+    }
+
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+        return res.status(400).json({
+        success: false,
+        message: "User already exists",
+        });
+    }
+
+    // Create new user
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+        name,
+        email,
+        password:hashedPassword,
+        role,
+    });
+
+    res.status(201).json({
+        success: true,
+        message: "User Registered Successfully",
+        user,
+    });
+
+    } catch (error) {
+    res.status(500).json({
+        success: false,
+        message: error.message,
+    });
+    }
+};
---
+++ server/server.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+console.log("MONGO_URI:", process.env.MONGO_URI);
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server Running on Port ${PORT}`);
+});
