6b5a5c07f32a1671a4064d337d07a0485273499f
Yogesh Maurya <yogeshmaurya1205@gmail.com>
2026-07-29 05:11:37 +0530

    Update API base URL to use environment variable

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: https://lms-3-xdu2.onrender.com/,
-  credential:true,
+    baseURL: import.meta.env.VITE_API_URL
 });
 
 api.interceptors.request.use((config) => {
