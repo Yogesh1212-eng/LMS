f5832f041feafc0b73285ba84bee472a685b8656
Yogesh Maurya <yogeshmaurya1205@gmail.com>
2026-07-30 00:09:55 +0530

    Configure CORS with dynamic frontend URL

--- server/app.js
+++ server/app.js
@@ -13,7 +13,12 @@ import quizRoutes from "./routes/quizRoutes.js";
 
 const app = express();
 
-app.use(cors({}));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/otp", otpRoutes);
