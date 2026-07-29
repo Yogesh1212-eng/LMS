dbcfad127f8dd4c5d3b3ba49f538ea17a7b1d3f8
Yogesh Maurya <yogeshmaurya1205@gmail.com>
2026-07-30 01:28:16 +0530

    Refactor DashboardLayout to use Navbar
    Replaced Sidebar and Topbar components with Navbar in DashboardLayout.

--- client/src/layouts/DashboardLayout.jsx
+++ client/src/layouts/DashboardLayout.jsx
@@ -1,22 +1,15 @@
 import { Outlet } from "react-router-dom";
-import Sidebar from "../components/dashboard/Sidebar";
-import Topbar from "../components/dashboard/Topbar";
+import Navbar from "../components/layout/Navbar";
 
 function DashboardLayout() {
   return (
-    <div className="flex min-h-screen bg-[#0B1120] text-white">
+    <div className="min-h-screen bg-[#0B1120]">
 
-      <Sidebar />
+      <Navbar />
 
-      <div className="flex-1 flex flex-col">
-
-        <Topbar />
-
-        <main className="flex-1 p-8">
-          <Outlet />
-        </main>
-
-      </div>
+      <main className="pt-20 p-6">
+        <Outlet />
+      </main>
 
     </div>
   );
